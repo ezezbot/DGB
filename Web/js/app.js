18ca6249ce4ea4b2ec32e9f057b21412c9f1733d
MoskalykA <45717749+ezezbot@users.noreply.github.com>
2020-01-08 21:35:01 +0100

    Update app.js

--- Web/js/app.js
+++ Web/js/app.js
@@ -1,7 +1,13 @@
 const app = new Vue({
-   el:'#app',
-   data: {
-      name: 'Name',
-      file: 'File'
-   }
+    el: '#app',
+    data: {
+        backdoors: ''
+    },
+    created() {
+        fetch('https://www.cray-dev.fr/dev/dev.json')
+            .then(response => response.json())
+            .then(json => {
+                this.backdoors = json.backdoors
+            })
+    }
 })
